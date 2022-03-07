7073d357303c3f87f284b38f5b8891d1deeaed58
G.Aravind <67952928+Aravind55597@users.noreply.github.com>
2022-03-07 22:00:45 +0800

    Initial commit

---
+++ src/main/java/com/remittancemiddleware/remittancemiddleware/RemittancemiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.remittancemiddleware.remittancemiddleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemittancemiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemittancemiddlewareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/remittancemiddleware/remittancemiddleware/RemittancemiddlewareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remittancemiddleware.remittancemiddleware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemittancemiddlewareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
