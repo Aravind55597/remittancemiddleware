49fbef4c58d58f8bec5838a3a7380383c9d9aa07
Xianweiii <68322946+Git-mind@users.noreply.github.com>
2022-03-19 01:23:32 +0800

    Added RemittanceTransactionController, Added getTransactionsByStatus api

---
+++ src/main/java/com/remittancemiddleware/remittancemiddleware/controller/RemittanceTransactionController.java
@@ -0,0 +1,37 @@
+package com.remittancemiddleware.remittancemiddleware.controller;
+
+import com.remittancemiddleware.remittancemiddleware.entity.transaction.RemittanceTransaction;
+import com.remittancemiddleware.remittancemiddleware.enums.TransactionStatus;
+import com.remittancemiddleware.remittancemiddleware.service.RemittanceTransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class RemittanceTransactionController {
+    private RemittanceTransactionService remittanceTransactionService;
+
+    @Autowired
+    public RemittanceTransactionController(RemittanceTransactionService theRemittanceTransactionService){
+        this.remittanceTransactionService = theRemittanceTransactionService;
+    }
+
+    @GetMapping("/remittanceTransaction/getTransactionsByStatus")
+    public List<RemittanceTransaction> getTransactionsByStatus(@RequestParam String status, @RequestParam int userId){
+        TransactionStatus transactionStatus = null;
+        if (status.equals("SUCCESSFUL")){
+            transactionStatus = TransactionStatus.SUCCESSFUL;
+        } else if (status.equals("PENDING_AML")){
+            transactionStatus = TransactionStatus.PENDING_AML;
+        } else if (status.equals("PENDING_COMPLIANCE_CHECKS")){
+            transactionStatus = TransactionStatus.PENDING_COMPLIANCE_CHECKS;
+        } else if (status.equals("REJECTED")){
+            transactionStatus = TransactionStatus.REJECTED;
+        }
+
+        List<RemittanceTransaction> remittanceTransaction = remittanceTransactionService.findByTransactionStatusAndCompanyId(transactionStatus, userId);
+        return remittanceTransaction;
+    }
+}
