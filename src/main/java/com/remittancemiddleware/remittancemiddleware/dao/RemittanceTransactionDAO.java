d3e99458d970da8568166cdc46c464689ed7fa7d
Xianweiii <68322946+Git-mind@users.noreply.github.com>
2022-03-19 01:21:45 +0800

    Added findByTransactionStatusAndCompanyId() method

--- src/main/java/com/remittancemiddleware/remittancemiddleware/dao/RemittanceTransactionDAO.java
+++ src/main/java/com/remittancemiddleware/remittancemiddleware/dao/RemittanceTransactionDAO.java
@@ -1,7 +1,11 @@
 package com.remittancemiddleware.remittancemiddleware.dao;
 
 import com.remittancemiddleware.remittancemiddleware.entity.transaction.RemittanceTransaction;
+import com.remittancemiddleware.remittancemiddleware.enums.TransactionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RemittanceTransactionDAO extends JpaRepository<RemittanceTransaction, Integer> {
+    List<RemittanceTransaction> findByTransactionStatusAndCompanyId(TransactionStatus status, int companyId);
 }
