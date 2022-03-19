d29ab6a048c5f766a0c944ba43dd8167d81bc1ab
Xianweiii <68322946+Git-mind@users.noreply.github.com>
2022-03-19 13:36:20 +0800

    Added RemittanceTransactionController  and RemittanceTransactionControllerImpl

---
+++ src/main/java/com/remittancemiddleware/remittancemiddleware/controller/RemittanceTransactionControllerImpl.java
@@ -0,0 +1,44 @@
+package com.remittancemiddleware.remittancemiddleware.controller;
+
+import com.remittancemiddleware.remittancemiddleware.entity.transaction.RemittanceTransaction;
+import com.remittancemiddleware.remittancemiddleware.enums.TransactionStatus;
+import com.remittancemiddleware.remittancemiddleware.service.RemittanceTransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/remittanceTransaction")
+public class RemittanceTransactionControllerImpl implements RemittanceTransactionController {
+    private RemittanceTransactionService remittanceTransactionService;
+
+    @Autowired
+    public RemittanceTransactionControllerImpl(RemittanceTransactionService theRemittanceTransactionService){
+        this.remittanceTransactionService = theRemittanceTransactionService;
+    }
+
+
+    @GetMapping("/getTransactionsByUser")
+    public List<RemittanceTransaction> getTransactionsByStatus(@RequestParam int userId){
+        List<RemittanceTransaction> remittanceTransaction = remittanceTransactionService.findByCompanyId(userId);
+        return remittanceTransaction;
+    }
+
+    @GetMapping("/getTransactionsByStatus")
+    public List<RemittanceTransaction> getTransactionsByStatus(@RequestParam String status, @RequestParam int userId){
+        TransactionStatus transactionStatus = null;
+        if (status.equals("SUCCESSFUL")){
+            transactionStatus = TransactionStatus.SUCCESSFUL;
+        } else if (status.equals("PENDING_AML")){
+            transactionStatus = TransactionStatus.PENDING_AML;
+        } else if (status.equals("PENDING_COMPLIANCE_CHECKS")){
+            transactionStatus = TransactionStatus.PENDING_COMPLIANCE_CHECKS;
+        } else if (status.equals("REJECTED")){
+            transactionStatus = TransactionStatus.REJECTED;
+        }
+
+        List<RemittanceTransaction> remittanceTransaction = remittanceTransactionService.findByTransactionStatusAndCompanyId(transactionStatus, userId);
+        return remittanceTransaction;
+    }
+}
